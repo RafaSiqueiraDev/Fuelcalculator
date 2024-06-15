d2d46a8adf95369089e2cb164a9e47ccd34e8958
Rafael Siqueira <FAELPSIQUEIRA@GMAIL.COM>
2024-06-15 21:08:30 +0100

    Criando novas telas

--- app/src/main/java/com/example/fuelcalculator/MainActivity.kt
+++ app/src/main/java/com/example/fuelcalculator/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.fuelcalculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btn_calcular = findViewById<Button>(R.id.btn_iniciar)
+
+
+        btn_calcular.setOnClickListener {
+
+            val intent = Intent(this, PriceActivity::class.java)
+            startActivity(intent)
+
+
         }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
